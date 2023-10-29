0fe3144af40df1b1f5c2619773efa7112383acd8
EunJung Jung <agjjung@naver.com>
2023-10-29 18:50:34 +0900

    ONB-71 style: ktlint formatting

--- presentation/src/main/java/com/yapp/bol/presentation/view/home/rank/HomeRankFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/home/rank/HomeRankFragment.kt
@@ -2,7 +2,6 @@ package com.yapp.bol.presentation.view.home.rank
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.net.Uri
 import android.os.Build
 import android.view.View
 import android.widget.Toast
