b381465429a151cd07eb7a69977134fb7550649e
EunJung Jung <agjjung@naver.com>
2023-12-10 11:36:02 +0900

    ONB-103 comment: design system의 msg, cancel, confirm dialog에 대한 kdoc 생성

--- designsystem/src/main/java/com/yapp/bol/designsystem/ui/dialog/MessageCancelConfirmDialog.kt
+++ designsystem/src/main/java/com/yapp/bol/designsystem/ui/dialog/MessageCancelConfirmDialog.kt
@@ -10,18 +10,34 @@ import com.yapp.bol.designsystem.databinding.DialogMessageCancelConfirmBinding
 import com.yapp.bol.designsystem.util.dialogWidthResize
 import com.yapp.bol.designsystem.util.createBoldSpannable
 
+/**
+ * @constructor [MessageCancelConfirmDialog]의 인스턴스 생성
+ * @param message 표시할 메시지를 나타내는 [SpannableStringBuilder].
+ * @param confirmMessage 확인 버튼의 텍스트를 나타내는 [String].
+ */
 class MessageCancelConfirmDialog(
     private val context: Context,
     private val message: SpannableStringBuilder,
     private val confirmMessage: String,
 ) : Dialog(context) {
 
+    /**
+     * @constructor [MessageCancelConfirmDialog]의 인스턴스 생성
+     * @param message 표시할 메시지를 나타내는 [String].
+     * @param confirmMessage 확인 버튼의 텍스트를 나타내는 [String].
+     */
     constructor(
         context: Context,
         message: String,
         confirmMessage: String,
     ) : this(context, SpannableStringBuilder(message), confirmMessage)
 
+    /**
+     * @constructor [MessageCancelConfirmDialog]의 인스턴스 생성
+     * @param originalMessage 표시할 메시지를 나타내는 [String].
+     * @param boldStringFromOriginal 볼드체로 표시될 섹션을 나타내는 [List]의 [String].
+     * @param confirmMessage 확인 버튼의 텍스트를 나타내는 [String].
+     */
     constructor(
         context: Context,
         originalMessage: String,
@@ -73,10 +89,20 @@ class MessageCancelConfirmDialog(
         context.dialogWidthResize(this, 0.9f)
     }
 
+    /**
+     * 확인 버튼에 대한 사용자 지정 클릭 리스너를 설정합니다.
+     *
+     * @param onConfirmClickListener 확인 버튼이 클릭될 때 수행할 동작을 나타내는 람다 함수.
+     */
     fun setOnConfirmClick(onConfirmClickListener: () -> Unit) {
         onConfirmClick = onConfirmClickListener
     }
 
+    /**
+     * 취소 버튼에 대한 사용자 지정 클릭 리스너를 설정합니다.
+     *
+     * @param onCancelClickListener 취소 버튼이 클릭될 때 수행할 동작을 나타내는 람다 함수.
+     */
     fun setOnCancelClick(onCancelClickListener: () -> Unit) {
         onCancelClick = onCancelClickListener
     }
