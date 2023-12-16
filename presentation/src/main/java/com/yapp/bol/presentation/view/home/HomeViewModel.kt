fcc91aa6d4fb0273039060f2efd947920b64914e
EunJung Jung <agjjung@naver.com>
2023-12-16 22:42:20 +0900

    ONB-112 feat: home을 가입한 그룹 없을 때에도 진입점으로 만들기 위해 groupId nullable로 변경

--- presentation/src/main/java/com/yapp/bol/presentation/view/home/HomeViewModel.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/home/HomeViewModel.kt
@@ -3,6 +3,6 @@ package com.yapp.bol.presentation.view.home
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-    var groupId: Long = 0
+    var groupId: Long? = null
     var gameId: Long? = null
 }
