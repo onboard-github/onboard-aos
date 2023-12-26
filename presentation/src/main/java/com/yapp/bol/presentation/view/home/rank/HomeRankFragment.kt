b7b0c96c2512df4a838c49496cc361f5ccbfc21b
EunJung Jung <agjjung@naver.com>
2023-12-26 21:58:11 +0900

    ONB-112 style: ktlint formatting

--- presentation/src/main/java/com/yapp/bol/presentation/view/home/rank/HomeRankFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/home/rank/HomeRankFragment.kt
@@ -46,12 +46,14 @@ class HomeRankFragment : BaseFragment<FragmentHomeRankBinding>(R.layout.fragment
     private lateinit var drawerGroupInfoAdapter: DrawerGroupInfoAdapter
     private lateinit var userRankGameAdapter: UserRankGameAdapter
     private val userRankAdapter by lazy { UserRankAdapter() }
-    private val groupChangeDialog by lazy { GroupChangeDialog (
-        onGroupClick = { viewModel.fetchAll(it) },
-        onSearchGroupClick = {
-            binding.root.findNavController().navigate(R.id.action_homeRankFragment_to_groupSearchFragment)
-        }
-    ) }
+    private val groupChangeDialog by lazy {
+        GroupChangeDialog(
+            onGroupClick = { viewModel.fetchAll(it) },
+            onSearchGroupClick = {
+                binding.root.findNavController().navigate(R.id.action_homeRankFragment_to_groupSearchFragment)
+            }
+        )
+    }
 
     override fun onViewCreatedAction() {
         super.onViewCreatedAction()
