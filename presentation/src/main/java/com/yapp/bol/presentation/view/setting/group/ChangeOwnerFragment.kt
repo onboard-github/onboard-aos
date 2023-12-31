81f286559d033424e0ce5fb8c9e408533c8f949e
EunJung Jung <agjjung@naver.com>
2024-01-01 02:37:55 +0900

    ONB-130 feat: group setting - change owner fragment base code 구현

---
+++ presentation/src/main/java/com/yapp/bol/presentation/view/setting/group/ChangeOwnerFragment.kt
@@ -0,0 +1,9 @@
+package com.yapp.bol.presentation.view.setting.group
+
+import com.yapp.bol.presentation.R
+import com.yapp.bol.presentation.base.BaseFragment
+import com.yapp.bol.presentation.databinding.FragmentChangeOwnerBinding
+
+class ChangeOwnerFragment : BaseFragment<FragmentChangeOwnerBinding>(R.layout.fragment_change_owner) {
+    // todo : ej 작업 부분
+}
