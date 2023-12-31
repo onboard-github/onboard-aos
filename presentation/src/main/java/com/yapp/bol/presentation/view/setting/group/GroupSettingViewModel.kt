effc7a759df92b7b6e927150d35d9580dafcb1e3
EunJung Jung <agjjung@naver.com>
2024-01-01 02:39:37 +0900

    ONB-130 feat: group setting activity base code 구현

---
+++ presentation/src/main/java/com/yapp/bol/presentation/view/setting/group/GroupSettingViewModel.kt
@@ -0,0 +1,20 @@
+package com.yapp.bol.presentation.view.setting.group
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GroupSettingViewModel @Inject constructor(
+
+) : ViewModel() {
+    private var groupId: Long = NEED_TO_SET
+
+    fun setGroupId(groupId: Long) {
+        this.groupId = groupId
+    }
+
+    companion object {
+        private const val NEED_TO_SET = -1L
+    }
+}
