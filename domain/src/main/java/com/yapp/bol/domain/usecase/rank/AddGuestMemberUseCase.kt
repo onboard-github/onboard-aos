4c381c5e34b49584e40aeab7817e39ae189b47a4
EunJung Jung <agjjung@naver.com>
2024-03-10 14:41:28 +0900

    ONB-237 feat: 홈에서 게스트 추가하는데 필요한 api usecase 추가

---
+++ domain/src/main/java/com/yapp/bol/domain/usecase/rank/AddGuestMemberUseCase.kt
@@ -0,0 +1,20 @@
+package com.yapp.bol.domain.usecase.rank
+
+import com.yapp.bol.domain.model.ApiResult
+import com.yapp.bol.domain.model.NicknameValidItem
+import com.yapp.bol.domain.repository.MemberRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class AddGuestMemberUseCase @Inject constructor(
+  private val repository: MemberRepository
+) {
+    suspend fun addGuest(groupId: Long, guestNickname: String) = repository.postGuestMember(
+        groupId.toInt(), guestNickname
+    )
+
+    fun checkNicknameValidation(groupId: Long, nickname: String): Flow<ApiResult<NicknameValidItem>> {
+        return repository.getValidateNickName(groupId.toInt(), nickname)
+    }
+
+}
